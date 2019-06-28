5bb1253bc545a472615a743b9f2f7f854e3a8583
adenuga558 <adenugaemma@gmail.com>
2019-06-28 14:02:14 +0100

    update

--- app/src/main/java/com/bluapp/detailview/MainActivity.java
+++ app/src/main/java/com/bluapp/detailview/MainActivity.java
@@ -29,14 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
     public ArrayList<MainData> getData() {
         ArrayList<MainData> list = new ArrayList<MainData>();
+        //Header one
         list.add(new MainData("Topic Header 1", R.drawable.coin_btc, "Title 1", "SubTitle 1"));
         list.add(new MainData("Topic Header 1", R.drawable.coin_dash, "Title 2", "SubTitle 2"));
+        list.add(new MainData("Topic Header 1", R.drawable.coin_btc, "Title 3", "SubTitle 3"));
+        list.add(new MainData("Topic Header 1", R.drawable.coin_dash, "Title 4", "SubTitle 4"));
+        list.add(new MainData("Topic Header 1", R.drawable.coin_dash, "Title 5", "SubTitle 5"));
+
+        //Header two
         list.add(new MainData("Topic Header 2", R.drawable.coin_etc, "Title 1", "SubTitle 1"));
         list.add(new MainData("Topic Header 2", R.drawable.coin_eth, "Title 2", "SubTitle 2"));
+
+        //Header three
         list.add(new MainData("Topic Header 3", R.drawable.coin_ltc, "Title 1", "SubTitle 1"));
         list.add(new MainData("Topic Header 3", R.drawable.coin_xrp, "Title 2", "SubTitle 2"));
+
+        ////Header four
         list.add(new MainData("Topic Header 4", R.drawable.coin_etc, "Title 1", "SubTitle 1"));
         list.add(new MainData("Topic Header 4", R.drawable.coin_eth, "Title 2", "SubTitle 2"));
+
+        //Header five
         list.add(new MainData("Topic Header 5", R.drawable.coin_xrp, "Title 1", "SubTitle 1"));
         list.add(new MainData("Topic Header 5", R.drawable.coin_ltc, "Title 2", "SubTitle 2"));
         return list;
